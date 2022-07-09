6735085c6c33c683696f1d6c38136d043bebdbf2
sandra-ntsoele <sandrantsoele@protonmail.com>
2022-07-09 14:37:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Supafire
+
+A quick guide on how to take advantage of Supabase's Row Level Security when using **Firebase Auth on Flutter**
---
+++ android/app/src/main/kotlin/com/example/supafire/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.supafire
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
