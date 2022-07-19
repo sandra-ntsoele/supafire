768e5848f85c2faf5aed3aaa4ab526589125531c
sandra-ntsoele <sandrantsoele@protonmail.com>
2022-07-19 10:29:51 +0200

    refactor: rename navigator service class

--- lib/services/navigation_service.dart
+++ lib/services/navigation_service.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class NavigatorService {
+class NavigationService {
   navigateTo(context, {required String routeName}) {
     Navigator.of(context).pushNamed(routeName);
   }
