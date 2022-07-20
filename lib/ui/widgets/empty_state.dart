ca898dcde7cb6ca9db2050d6361088c6f39eff6c
sandra-ntsoele <sandrantsoele@protonmail.com>
2022-07-20 15:09:46 +0200

    feat: create an empty state widget

---
+++ lib/ui/widgets/empty_state.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:supafire/ui/shared/layout_helpers.dart';
+import 'package:supafire/ui/shared/shared_styles.dart';
+
+class EmptyState extends StatelessWidget {
+  const EmptyState({Key? key}) : super(key: key);
+
+  final String url =
+      "https://ouch-cdn2.icons8.com/QAh22NFGyC0EsoCo-yI98CCZE0F4eh18O7uO8vVBG0A/rs:fit:375:456/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvOTE3/LzgyMDU5N2Y4LTVh/M2EtNDEwNS05YjQx/LTk0NTcwMThmNmFh/Zi5zdmc.png";
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image(
+          image: NetworkImage(url),
+          fit: BoxFit.cover,
+          width: size.width * 0.3,
+        ),
+        LayoutHelpers.largeVerticalSpace,
+        Text(
+          "No books found.\nEmpty State",
+          style: SharedStyles.headingOne,
+          textAlign: TextAlign.center,
+        ),
+        LayoutHelpers.smallVerticalSpace,
+        Text(
+          "Click the 'create button' to add the first book you want to read",
+          style: SharedStyles.paragraphTwo,
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+}
