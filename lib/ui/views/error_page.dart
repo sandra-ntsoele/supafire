e1bbb8b6a6e232ab3185f9d6f80a3d73bc927064
sandra-ntsoele <sandrantsoele@protonmail.com>
2022-07-22 21:35:03 +0200

    feat: create and error page

---
+++ lib/ui/views/error_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:supafire/controllers/home_page_controller.dart';
+import 'package:supafire/ui/shared/layout_helpers.dart';
+import 'package:supafire/ui/shared/shared_styles.dart';
+
+class ErrorPage extends StatelessWidget {
+  final HomePageController viewController;
+  const ErrorPage({
+    Key? key,
+    required this.viewController,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          "Something went wrong.",
+          style: SharedStyles.headingOne,
+        ),
+        LayoutHelpers.smallVerticalSpace,
+        RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            style: SharedStyles.paragraphTwo,
+            text: "We had difficulty connecting to the database\n",
+            children: [
+              const TextSpan(text: "Reload the page or\n"),
+              TextSpan(
+                  text: "Contact support",
+                  style: SharedStyles.hyperlink,
+                  recognizer: TapGestureRecognizer()..onTap = () => InkWell()),
+            ],
+          ),
+        ),
+        LayoutHelpers.mediumVerticalSpace,
+        ElevatedButton(
+          onPressed: () => viewController.reloadPage(),
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(const CircleBorder()),
+          ),
+          child: const Icon(Icons.replay_outlined),
+        ),
+      ],
+    );
+  }
+}
