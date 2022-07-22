f9f17bb5b91947a18085e85d6370a99816c93d07
sandra-ntsoele <sandrantsoele@protonmail.com>
2022-07-22 21:31:53 +0200

    refactor: check if email has been verified before running set state

--- lib/controllers/verify_email_page_controller.dart
+++ lib/controllers/verify_email_page_controller.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:supafire/ui/views/home_view.dart';
 import 'package:supafire/ui/views/verify_email_page.dart';
@@ -9,26 +8,26 @@ import 'package:supafire/services/authentication_service.dart';
 class VerifyEmailPageController extends State<VerifyEmailPage> {
   final AuthenticationService _authService = AuthenticationService();
 
-  User? user;
   Timer? _timer;
-  late bool emailVerified;
+  bool emailVerified = false;
 
   @override
   void initState() {
     super.initState();
 
-    user = _authService.currentUser;
-    emailVerified = false;
-
     _timer = Timer.periodic(
       const Duration(seconds: 1),
       (timer) async {
         if (mounted) {
-          await _authService.currentUser!.reload().then(
-                (value) => setState(
-                  () => emailVerified = user!.emailVerified,
-                ),
-              );
+          await _authService.currentUser!.reload().whenComplete(
+            () {
+              if (_authService.currentUser!.emailVerified) {
+                setState(() {
+                  emailVerified = true;
+                });
+              }
+            },
+          );
         }
       },
     );
