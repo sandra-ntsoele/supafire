556a662eea9a151a1fb2c1ff834e1decec03dffb
sandra-ntsoele <sandrantsoele@protonmail.com>
2022-07-22 18:46:47 +0200

    feat: wrap page render function with a StreamBuilder that stream data that from the wishlist table

--- lib/controllers/home_page_controller.dart
+++ lib/controllers/home_page_controller.dart
@@ -2,7 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:supafire/constants/route_names.dart';
 import 'package:supafire/services/authentication_service.dart';
+import 'package:supafire/services/database_service.dart';
 import 'package:supafire/services/navigation_service.dart';
+import 'package:supafire/ui/views/error_page.dart';
 import 'package:supafire/ui/views/home_view.dart';
 import 'package:supafire/ui/views/profile_page.dart';
 import 'package:supafire/ui/views/signup_view.dart';
@@ -27,23 +29,53 @@ class HomePageController extends State<HomePage> {
         },
       );
 
+  /// Class fields
+  final AuthenticationService _authService = AuthenticationService();
+  final NavigationService _navService = NavigationService();
+
+  Future? syncUser;
+  int currentIndex = 0;
+  List<Widget> pages = [];
+
   @override
   void initState() {
     pages.addAll([
       const BookCollectionPage(),
-      const ProfilePage(),
+      ProfilePage(viewController: this),
     ]);
+
+    syncUser = DatabaseService().syncUser(
+      displayName: _authService.currentUser!.displayName,
+      email: _authService.currentUser!.email,
+      uid: _authService.currentUser!.uid,
+    );
 
     super.initState();
   }
 
-  /// Class fields
-  final AuthenticationService _authService = AuthenticationService();
-  final NavigationService _navService = NavigationService();
-  int currentIndex = 0;
-  List<Widget> pages = [];
-
   /// Class methods
+  Widget selectedPage() {
+    return StreamBuilder(
+      stream: DatabaseService().tableStream(
+        table: "wishlist",
+        uniqueColumns: "book_id, user_id, created, book_is_read",
+      ),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          if (snapshot.hasError) {
+            return ErrorPage(viewController: this);
+          } else {
+            return pages[currentIndex];
+          }
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
   void signOut() async {
     await _authService.signOut().then(
           (value) => _navService.navigateTo(
@@ -59,7 +91,5 @@ class HomePageController extends State<HomePage> {
     });
   }
 
-  Widget selectedPage() {
-    return pages[currentIndex];
-  }
+  reloadPage() => setState(() {});
 }
