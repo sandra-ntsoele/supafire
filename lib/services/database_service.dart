5d99eae7e646b5da8e355371c8346dfe9c3f51f9
sandra-ntsoele <sandrantsoele@protonmail.com>
2022-07-25 09:11:05 +0200

    fix: row level security error by calling setAuth on supabase

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -17,30 +17,11 @@ class DatabaseService {
     return subscription.map((event) => event);
   }
 
-  _setAccessToken() async {
+  setAccessToken() async {
     final firebaseToken = await _authService.idToken;
     Map payload = JwtDecoder.decode(firebaseToken);
-    JWT(payload).sign(SecretKey(DBDetails.secret));
-  }
+    String accessToken = JWT(payload).sign(SecretKey(DBDetails.secret));
 
-  Future syncUser({
-    String? displayName,
-    String? email,
-    String? uid,
-  }) async {
-    await _setAccessToken();
-    PostgrestResponse response = await supabase.from("users").insert({
-      "display_name": displayName,
-      "email": email,
-      "user_id": uid,
-    }, returning: ReturningOption.minimal).execute();
-
-    if (response.hasError) {
-      log(response.error.toString());
-      // code
-      return response.error;
-    } else {
-      return response;
-    }
+    supabase.auth.setAuth(accessToken);
   }
 }
